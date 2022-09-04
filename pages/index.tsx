b82fdc939058273961bbe76c33b106b69d8612f2
moizbinata <abdulmoiz6789@gmail.com>
2022-09-04 23:03:57 +0500

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,81 @@
 import type { NextPage } from 'next'
+import React, { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
+import * as Fa from 'react-icons/fa'
+// import { BeakerIcon } from '@heroicons/react/solid'
+
 
 const Home: NextPage = () => {
+  const [selects, setSelects] = useState<any | null>(null);
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <header>
+        <div className='text-left font-bold'>
+          <span className='text-green-500 text-left'>sLoan</span>App
         </div>
-      </main>
+      </header>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+          <div className="w-3/5 rounded-tl-2xl rounded-bl-2xl">
+            <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+              <Image
+                alt='Mountains'
+                src='/image/1.jpg'
+                layout='fill'
+                objectFit='cover'
+              />
+            </div>
+          </div>
+          <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+            <h2 className='text-3xl font-bold mb-2'>Hello friend</h2>
+            <div className='border-2 w-10 border-white inline-block mb-2'>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+            </div>
+            <p className='mb-10'>Fill up form</p>
+            <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+              <Fa.FaEnvelope className='text-gray-400 m-2' />
+              <input type='email' name='email' placeholder='Email' className='bg-gray-100 text-black outline-none text-sm flex-1' />
+            </div>
+            <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+              <Fa.FaPhone className='text-gray-400 m-2' />
+              <input type='phone' name='phone' placeholder='Phone' className='bg-gray-100 text-black outline-none text-sm flex-1' />
+            </div>
+            <p className='text-sm text-left'>Loan Request Amount</p>
+            <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+              <Fa.FaDollarSign className='text-gray-400 m-2' />
+              <input type='number' name='amount' placeholder='Loan Amount USD' className='bg-gray-100 text-black outline-none text-sm flex-1' />
+            </div>
+            <p className='text-sm text-left'>Installment Plan in year</p>
+            <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+              <Fa.FaCalendar className='text-gray-400 m-2' />
+              {/* <input type='email' name='email' placeholder='Email' className='bg-gray-100 outline-none text-sm flex-1' /> */}
+              <select value={selects} placeholder='Installment Plan in year' className="text-black block bg-gray-100 outline-none text-sm flex-1" onChange={e => setSelects(e.target.value)}>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <p className='text-sm text-left'>Monthly Installment to be</p>
+            <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+              <Fa.FaDollarSign className='text-gray-400 m-2' />
+              <input type='number' readOnly name='amount' placeholder='Loan Amount USD' className='bg-gray-100 text-black outline-none text-sm flex-1' />
+            </div>
+
+
+            <a href="#" className='border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500'>Request Loan</a>
+          </div>
+        </div>
+      </main >
+
+
+    </div >
   )
 }
 
